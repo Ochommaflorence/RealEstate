e9044471ca8f3ca7dbf181da0356e8ad5f9fa477
chizzy01 <elvischizzy09@gmail.com>
2021-09-07 15:42:41 +0100

    first commit

---
+++ src/Pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>This is about us</h1>
+        </div>
+    )
+}
---
+++ src/Pages/Property.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Property = () => {
+  return (
+    <div>
+      <h1>this is property page</h1>
+    </div>
+  );
+}
+
+export default Property;
