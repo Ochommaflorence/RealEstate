ff64d812aa53521885bfeb0b58efa91cdbad5289
chizzy01 <elvischizzy09@gmail.com>
2021-09-07 15:55:52 +0100

    adding some files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
- import Home from './Pages/Home';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Rentals from "./Pages/Rentals";
+import Property from "./Pages/Property";
+
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Navbar/>
-       <Switch>
-        <Route>
-          <Home/>
-        </Route>
-       </Switch>
-     </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route>
+            <Home />
+          </Route>
+          <Route>
+            <About />
+          </Route>
+          <Route>
+            <Rentals />
+          </Route>
+          <Route>
+            <Property />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
