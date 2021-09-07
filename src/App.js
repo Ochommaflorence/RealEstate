dd0daea62abbb99c69286d7c7e60b91c3766f5e7
chizzy01 <elvischizzy09@gmail.com>
2021-09-07 15:53:40 +0100

    adding some new files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
- 
+ import Home from './Pages/Home';
 function App() {
   return (
     <div className="App">
      <Router>
        <Navbar/>
+       <Switch>
+        <Route>
+          <Home/>
+        </Route>
+       </Switch>
      </Router>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,26 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div>
-      
+      <Nav>
+          <Logo>Florrie's Home</Logo>
+          <MenuBars/>
+          <NavMenu>
+              <ul>
+                  <li>
+                      <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                      <Link to="/rentals">Rentals</Link>
+                  </li>
+                  <li>
+                      <Link to="/property">Property</Link>
+                  </li>
+              </ul>
+          </NavMenu>
+      </Nav>
     </div>
   );
 }
