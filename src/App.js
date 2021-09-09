92a25c919cfd05a64fd485488800516062241112
chizzy01 <elvischizzy09@gmail.com>
2021-09-09 14:23:41 +0100

    adding a new file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+     <Navbar/>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
       <div className="navbar">
-          <div className="navbar-container container">
-           <Link to="/" className="navbar-logo">
-               Florrie's <span>Home</span>
-           </Link>
-           <div className="menu-icon"></div>
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo">
+            <FaHome className="navbar-icon" />
+            Florrie's <span>Home</span>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/property" className="nav-links">
+                Property
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/rentals" className="nav-links">
+                Rentals
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default Navbar;
