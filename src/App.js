2007f2f6175280e65c36c87f4f905016d1f4f8fe
chizzy01 <elvischizzy09@gmail.com>
2021-09-09 13:52:53 +0100

    adding some new files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Rentals from "./Pages/Rentals";
-import Property from "./Pages/Property";
-import Contact from "./Pages/Contact";
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact to="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/rentals">
-            <Rentals />
-          </Route>
-          <Route path="/property">
-            <Property />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </Router>
+     
     </div>
   );
 }
--- src/Pages/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const About = () => {
-    return (
-        <div>
-            <h1>This is about us</h1>
-        </div>
-    )
-}
