3cbc5bcad3aee49c0d5aadd3dd0011acfc867405
chizzy01 <elvischizzy09@gmail.com>
2021-09-14 10:50:57 +0100

    committing files are on going

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Rental from "./components/Pages/Rental";
+import Property from "./components/Pages/Property";
+import Contact from "./components/Pages/Contact";
 
 
 function App() {
@@ -8,6 +13,23 @@ function App() {
    <>
      <Router>
      <Navbar/>
+     <Switch>
+       <Route exact path="/">
+         <Home/>
+       </Route>
+       <Route path="/about">
+         <About/>
+       </Route>
+    <Route path="/about">
+         <Rental/>
+       </Route>
+       <Route path="/about">
+        <Property/>
+       </Route>
+       <Route path="/about">
+         <Contact/>
+       </Route>
+     </Switch>
      </Router>
    </>
   );
