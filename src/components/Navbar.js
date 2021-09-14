660a3c34f547d41dc1dd5d084a12f93dd216790b
chizzy01 <elvischizzy09@gmail.com>
2021-09-14 11:30:11 +0100

    committing files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./Navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-  <nav className="navbar">
-    <div className="nav-container">
-      <Link exact to="/" className="nav-logo">
-        Florrie's Home
-      </Link>
-      <ul className="nav-menu">
-        <li className="nav-item">
-        <Link to="/" className="nav-links">
-        Home
-      </Link>
-        </li>
-        <li className="nav-item">
-        <Link to="/about" className="nav-links">
-        About
-      </Link>
-        </li>
-        <li className="nav-item">
-        <Link to="/property" className="nav-links">
-        Property
-      </Link>
-        </li>
-        <li className="nav-item">
-        <Link to="/rental" className="nav-links">
-        Rentals
-      </Link>
-        </li>
-        <li className="nav-item">
-        <Link to="/contact" className="nav-links">
-        Contact Us
-      </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+      <nav className="navbar">
+        <div className="nav-container">
+          <Link exact to="/" activeClassName="active" className="nav-logo">
+            Florrie's Home
+          </Link>
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link to="/" activeClassName="active" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" activeClassName="active" className="nav-links">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/property"
+                activeClassName="active"
+                className="nav-links"
+              >
+                Property
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/rental" activeClassName="active" className="nav-links">
+                Rentals
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+              >
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
-}
+};
 
 export default Navbar;
